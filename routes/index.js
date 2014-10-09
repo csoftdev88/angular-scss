34086c1819d33cf124020b86616c61c31007432e
oilart <alex.khrapko@gmail.com>
2014-10-09 17:53:20 +0200

    cz route

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,11 @@
 module.exports = function (app) {
+
+    app.get('/cs-cz', function (req, res, next) {
+        res.render('index-cs_CZ', {
+            title: 'Express'
+        });
+    });
+
     app.get('/', function (req, res, next) {
         res.render('index-en_US', {
             title: 'Express'
