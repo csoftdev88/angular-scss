6be1906e4e80e8d647997b9cafaa55000a673dfe
Jan Dolezel <jando@salsitasoft.com>
2015-05-14 10:06:28 +0200

    Moving to angular 1.3

--- build.config.js
+++ build.config.js
@@ -36,17 +36,30 @@ module.exports = {
       /* These files should be loaded first in order to initialize app namespaces */
       jsunit_init: [
         'bower_components/angular/angular.js',
-        'bower_components/angular-route/angular-route.js',
         'bower_components/angular-resource/angular-resource.js',
         'bower_components/angular-ui-router/release/angular-ui-router.min.js',
         'bower_components/angular-touch/angular-touch.min.js',
         'bower_components/angular-chosen-localytics/chosen.js',
-        'bower_components/jquery/jquery.js',
+        'bower_components/jquery/dist/jquery.min.js',
         'bower_components/angular-bootstrap/ui-bootstrap.min.js',
         'bower_components/angular-bootstrap/ui-bootstrap-tpls.min.js',
         'bower_components/chai/chai.js',
         'bower_components/angular-mocks/angular-mocks.js',
-        'bower_components/sinon/index.js',
+        'bower_components/sinon/lib/sinon.js',
+        'bower_components/sinon/lib/sinon/util/core.js',
+        'bower_components/sinon/lib/sinon/extend.js',
+        'bower_components/sinon/lib/sinon/spy.js',
+        'bower_components/sinon/lib/sinon/call.js',
+        'bower_components/sinon/lib/sinon/behavior.js',
+        'bower_components/sinon/lib/sinon/stub.js',
+        'bower_components/sinon/lib/sinon/mock.js',
+        'bower_components/sinon/lib/sinon/collection.js',
+        'bower_components/sinon/lib/sinon/assert.js',
+        'bower_components/sinon/lib/sinon/sandbox.js',
+        'bower_components/sinon/lib/sinon/test.js',
+        'bower_components/sinon/lib/sinon/test_case.js',
+        'bower_components/sinon/lib/sinon/typeOf.js',
+        'bower_components/sinon/lib/sinon/match.js',
         'bower_components/sinon-chai/lib/sinon-chai.js',
         'karma/init.js',
         'src/app/**/*.js'
@@ -56,9 +69,8 @@ module.exports = {
     /* List of vendor files used in application */
     vendor_files: {
       js: [
-        'bower_components/jquery/jquery.js',
+        'bower_components/jquery/dist/jquery.min.js',
         'bower_components/angular/angular.js',
-        'bower_components/angular-route/angular-route.js',
         'bower_components/angular-resource/angular-resource.js',
         'bower_components/angular-touch/angular-touch.min.js',
         'bower_components/angular-chosen-localytics/chosen.js',
@@ -70,7 +82,7 @@ module.exports = {
         'bower_components/underscore/underscore.js',
         'bower_components/angular-underscore-module/angular-underscore-module.js',
         'bower_components/angular-translate/angular-translate.min.js',
-        'bower_components/angular-input-match/dist/angular-input-match.js'
+        'bower_components/angular-validation-match/dist/angular-input-match.js'
       ],
       styles: []
     }
