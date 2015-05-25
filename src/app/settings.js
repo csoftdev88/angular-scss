184db8b8458af02e957521de03b683f068e74bcf
Lubos Turek <lubos@salsitasoft.com>
2015-05-25 13:12:11 +0000

    New API address

--- src/app/settings.js
+++ src/app/settings.js
@@ -4,7 +4,7 @@ angular.module('mobiusApp.config', [])
 
 .constant('Settings', {
   'API': {
-    'baseURL': 'http://52.6.221.79:3010/api/2.7.1/',
+    'baseURL': 'http://52.5.129.243:3010/api/2.7.1/',
     'contents' : 'contents',
     'generics': {
       'currencies': 'generics/currencies',
