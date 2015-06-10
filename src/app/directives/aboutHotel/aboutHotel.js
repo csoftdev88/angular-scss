54a99aa7d53e67879719936b669115ba12082523
oilart <oilart@live.com>
2015-06-10 11:58:55 +0200

    about hotel - expand, updated dynamic directive

--- src/app/directives/aboutHotel/aboutHotel.js
+++ src/app/directives/aboutHotel/aboutHotel.js
@@ -5,7 +5,9 @@ angular.module('mobiusApp.directives.aboutHotel', [])
 .directive('aboutHotel', function(){
   return {
     restrict: 'E',
-    scope: {},
-    templateUrl: 'directives/aboutHotel/aboutHotel.html'
+    templateUrl: 'directives/aboutHotel/aboutHotel.html',
+    link: function(scope){
+      scope.isCollapsed = true;
+    }
   };
 });
