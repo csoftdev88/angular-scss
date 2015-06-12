9ecb553b35ddb6bce93c3b2f00b0d01b943fc558
Verca <veronika.crkvova@gmail.com>
2015-06-12 14:02:07 +0200

    fixed merge conflicts

--- src/app/controllers/modals/loyalties/badges.js
+++ src/app/controllers/modals/loyalties/badges.js
@@ -1,26 +1,21 @@
 'use strict';
 /*
-* This module controls a modal which displays a list of badges
-*/
+ * This module controls a modal which displays a list of
+ * badges/loyalty cards
+ */
 angular.module('mobius.controllers.modals.loyalties.badges', [])
 
-// NOTE: (ALEX) It seems like loyalties/badges dialogues will have the same functionality
-// so we can use the same controller for two dialogues
-// TODO: Check ^^^
 .controller( 'BadgesCtrl', function($scope, $controller, $modalInstance,
-  Settings, data) {
+  Settings, data, _) {
 
     $controller('ModalDataCtrl', {$scope: $scope, $modalInstance: $modalInstance, data: data});
-
     // NOTE: put this back if some of the badges have pure HTML content
     //$controller('SanitizeCtrl', {$scope: $scope});
-    $scope.items = data;
-    if(data.length){
-      $scope.selectItem(data[0]);
-    }
 
-   	$scope.selectItem = function(item){
-      $scope.selectedItem = item;
-   	};
+    $scope.badges = data;
+    $scope.selectedBadge = !_.isEmpty($scope.badges) ? $scope.badges[0] : null;
+    $scope.showBadgeDetail = function(index) {
+      $scope.selectedBadge = $scope.badges[index];
+    };
   }
 );
