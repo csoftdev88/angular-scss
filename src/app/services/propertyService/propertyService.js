3c1e4325874d4846118fc745185053b8a064f93b
Lubos Turek <lubos@salsitasoft.com>
2015-06-18 08:02:06 +0000

    Took out most of the mocks

--- src/app/services/propertyService/propertyService.js
+++ src/app/services/propertyService/propertyService.js
@@ -3,44 +3,13 @@
 * This service gets content for application main menu
 */
 angular.module('mobiusApp.services.properties', [])
-.service( 'propertyService',  function(apiService, $q) {
-
-  var mock = {
-    'name' : 'Queen room',
-    'code' : 'QWN',
-    'description' : 'Standard Room, 1 Queen bed, Sitting Area, Free Internet, Parking',
-    'numberOfBeds' : 1,
-    'maxGuests' : 2,
-    'bedTypeName' : 'Queen',
-    'priceFrom': 199,
-    'size': {
-      'from': 20,
-      'to': 30,
-      'unit': 'sq. feet'
-    },
-    'amenities' : [
-      {'name': '24-hour coffee shop', 'icon': 'http://www.images.com/default11.jpg'},
-      {'name': 'Parking', 'icon': 'http://www.images.com/default11.jpg'}
-    ],
-    'images' : [
-      {
-        'uri' : 'http://www.images.com/default11.jpg',
-        'alt': 'picture1'
-      },
-      {
-        'uri' : 'http://www.images.com/default12.jpg',
-        'alt': 'picture2'
-      }
-    ],
-    'IRI' : '/properties/ABB/rooms/QWN/'
-  };
+.service( 'propertyService',  function(apiService) {
 
   function getAll(params){
     return apiService.get(apiService.getFullURL('properties.all'), params);
   }
 
   function getPropertyDetails(propertyCode, params){
-    debugger;
     var URL = apiService.getFullURL('properties.details', {propertyCode: propertyCode});
     return apiService.get(URL, params);
   }
@@ -51,14 +20,11 @@ angular.module('mobiusApp.services.properties', [])
   }
 
   function getRoomDetails(propertyCode, roomTypeCode){
-    return $q.when(mock);
-    /*
     var URL = apiService.getFullURL('properties.room.details', {
       propertyCode: propertyCode,
       roomTypeCode: roomTypeCode
     });
     return apiService.get(URL);
-    */
   }
 
   function getRoomProductDetails(propertyCode, roomTypeCode, params){
