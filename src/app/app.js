442c0fe6116b64268da5be00bb5ded6d5bf0fc56
Jan Dolezel <jando@salsitasoft.com>
2015-06-19 15:40:26 +0200

    Fixed notreloading and url params

--- src/app/app.js
+++ src/app/app.js
@@ -106,7 +106,7 @@ angular
         controller: 'MainCtrl',
         // NOTE: These params are used by booking widget
         // Can be placed into induvidual state later if needed
-        url: '?property&region&children&adults&dates&rate&rooms'
+        url: '?property&region&children&adults&dates&rate&rooms&groupCode&corpCode&promoCode'
       })
 
       // Home page
