8144baa2ea04902d20df33a327f153f4db9c1aac
Jan Dolezel <jando@salsitasoft.com>
2015-06-24 16:21:11 +0200

    Offers moved

--- src/app/controllers/offers.js
+++ src/app/controllers/offers.js
@@ -1,13 +1,36 @@
 'use strict';
-
+/*
+ * This module controlls offers page
+ */
 angular.module('mobius.controllers.offers', [])
 
-.controller( 'OffersCtrl',  function($scope, $stateParams, $controller, breadcrumbsService) {
+  .controller('OffersCtrl', function($scope, $controller, contentService, $stateParams, _, breadcrumbsService){
 
-  $controller('MainCtrl', {$scope: $scope});
-  breadcrumbsService.addBreadCrumb('Offers');
+    $controller('MainCtrl', {$scope: $scope});
+    breadcrumbsService.addBreadCrumb('Offers');
 
-  // Application settings
-  $scope.category = $stateParams.category;
-  $scope.offerID = $stateParams.offerID;
-});
+    var NUMBER_OF_RELEVANT_OFFERS = 3;
+
+    var selectedOfferIndex;
+
+    $scope.showDetail = $stateParams.$stateParams.showDetail;
+
+    contentService.getOffers().then(function(response) {
+      $scope.offersList = response;
+      selectedOfferIndex =  _.findIndex($scope.offersList, function(item) {
+        return item.code === $stateParams.$stateParams.code;
+      });
+    });
+
+    $scope.getRelevant = function(offer, index) {
+      var offset = selectedOfferIndex < NUMBER_OF_RELEVANT_OFFERS ? 1 : 0;
+      return selectedOfferIndex !== index && NUMBER_OF_RELEVANT_OFFERS + offset > parseInt(index, 10);
+    };
+
+    $scope.selectOffer = function(index) {
+      $scope.selectedOffer = $scope.offersList[index];
+      selectedOfferIndex = index;
+      $scope.showDetail = true;
+    };
+
+  });
