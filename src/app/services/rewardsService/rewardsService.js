100c3dac073f02fad6942bcf9837a4aa506e867b
oilart <alex.khrapko@gmail.com>
2015-07-06 17:56:25 +0200

    fixed rewards API integration

--- src/app/services/rewardsService/rewardsService.js
+++ src/app/services/rewardsService/rewardsService.js
@@ -3,21 +3,12 @@
 * This service gets content for application main menu
 */
 angular.module('mobiusApp.services.rewards', [])
-.service( 'rewardsService',  function(apiService, $q) {
+.service( 'rewardsService',  function(apiService) {
 
   function getAll(userId){
-
-    var q = $q.defer();
-
-    apiService.get(apiService.getFullURL('rewards.all', {
+    return apiService.get(apiService.getFullURL('rewards.all'), {
       customerId: userId
-    })).then(function(data){
-      q.resolve(data);
-    }, function(error){
-      q.reject(error);
     });
-
-    return q.promise;
   }
 
   function getRewardDetails(rewardCode, params){
