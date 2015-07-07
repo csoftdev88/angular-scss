7ea42b77ff9daa5499bf3ff42da3480e6cd0b85a
oilart <alex.khrapko@gmail.com>
2015-07-07 14:45:41 +0200

    info message on empty lists

--- src/app/controllers/rewards/rewards.js
+++ src/app/controllers/rewards/rewards.js
@@ -26,7 +26,7 @@ angular.module('mobius.controllers.rewards', [])
         rewardsService.getConsumable(user.getCustomerId()),
       ]).then(function(data){
         console.log(data);
-        $scope.consumedRewards = data[0];
+        $scope.consumedRewards = [] || data[0];
         $scope.consumableRewards = data[1].map(function(reward){
           // Adding affordable flag
           reward._isAffordable = user.getUser().loyalties.amount >= reward.pointCost;
@@ -69,4 +69,6 @@ angular.module('mobius.controllers.rewards', [])
         preloaderFactory(buyPromise);
       });
     };
+
+    $scope.viewMode = 'consumed';
   });
