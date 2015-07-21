00e82d10c55a9b97445cbcd889f4eba1cdb03a0f
Artur Fenko <artur.fenko@itechart-group.com>
2015-07-21 21:15:26 +0300

    normalised links

--- src/app/controllers/common/content/content.js
+++ src/app/controllers/common/content/content.js
@@ -79,8 +79,8 @@ angular.module('mobius.controllers.common.content', [])
 
       var params = {};
       params[$scope.settings.paramName] = code;
-
-      return $state.href(code?$scope.settings.detailState:$scope.settings.listState, params);
+      var link = $state.href(code?$scope.settings.detailState:$scope.settings.listState, params);
+      return (link && link.endsWith('/')) ? link.slice(0,-1) : link;
     };
 
     $scope.goToState = function($event, code){
