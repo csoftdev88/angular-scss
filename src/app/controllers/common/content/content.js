2362ea09ff86971d163318d3767a7eb8094ad774
Artur Fenko <artur.fenko@itechart-group.com>
2015-07-26 22:46:29 +0300

    booking widget: pre-fill property

--- src/app/controllers/common/content/content.js
+++ src/app/controllers/common/content/content.js
@@ -2,8 +2,8 @@
 
 angular.module('mobius.controllers.common.content', [])
 
-  .controller('ContentCtr', function($scope, propertyService, contentService,
-         locationService, $state, _) {
+  .controller('ContentCtr', function($scope, preloaderFactory, propertyService, contentService,
+         locationService, bookingService, $state, _) {
 
     // We are using different methods for getting the data
     // from the server according to content type. Also, menu
@@ -75,13 +75,47 @@ angular.module('mobius.controllers.common.content', [])
 
     $scope.settings = contentTypes[$scope.item];
 
+    // Getting the details from booking widget
+    var bookingParams = bookingService.getAPIParams(true);
+
+    // Loading hotels
+    var hotelsPromise = propertyService.getAll(bookingParams).then(function(hotels){
+      $scope.hotels = hotels || [];
+    });
+
+    preloaderFactory(hotelsPromise);
+
+    function findPropertyBySlug(value) {
+      var obj;
+      if ($scope.hotels instanceof Array) {
+        obj = _.find($scope.hotels, function (item) {
+          return item && item.meta &&
+            item.meta.slug && item.meta.slug === value;
+        });
+      }
+      return obj;
+    }
+
+    function createParamsObject(code) {
+      var params = {};
+      params[$scope.settings.paramName] = code;
+      preprocessParams(code, params);
+      return params;
+    }
+
+    function preprocessParams(code, params) {
+      //if hotel slug then need to update property param
+      if (contentTypes.hotels.paramName === $scope.settings.paramName && code) {
+        var property = findPropertyBySlug(code);
+        params.property = property ? property.code : null;
+      }
+    }
+
     $scope.getStateHref = function(code){
       if(!$scope.settings){
         return null;
       }
-
-      var params = {};
-      params[$scope.settings.paramName] = code;
+      var params = createParamsObject(code);
       var link = $state.href(code?$scope.settings.detailState:$scope.settings.listState, params);
       return (link && link.substr(-1) === '/') ? link.slice(0,-1) : link;
     };
@@ -90,13 +124,9 @@ angular.module('mobius.controllers.common.content', [])
       if(!$scope.settings){
         return null;
       }
-
-      var params = {};
-      params[$scope.settings.paramName] = code;
-
+      var params = createParamsObject(code);
       $event.preventDefault();
       $event.stopPropagation();
-
       $state.go(code?$scope.settings.detailState:$scope.settings.listState, params, {reload: true});
     };
 
