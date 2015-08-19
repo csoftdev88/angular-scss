41c1e74c279fdd9d273577aa578caa732e2d08b8
oilart <alex.khrapko@gmail.com>
2015-08-19 14:49:29 +0200

    room products - unit tests

--- src/app/directives/roomProducts/roomProducts.spec.js
+++ src/app/directives/roomProducts/roomProducts.spec.js
@@ -5,14 +5,17 @@ describe('RoomProducts', function() {
   var TEMPLATE_URL = 'directives/roomProducts/roomProducts.html';
   var TEMPLATE_CONTENT = '<div>room products</div>';
 
-  var TEST_ROOM_PRODUCTS = [
-    {
-      productId: 123
+  var TEST_ROOM_PRODUCTS = {
+    data: {
+      products: [
+        {code: 123}
+      ]
     }
-  ];
+  };
 
   var _rootScope, _scope, _elem, _templateCache, _spyTemplateCacheGet,
-    _spyOpenPoliciesInfo;
+    _spyOpenPoliciesInfo, _spyOpenRoomDetailsDialog, _spyOpenPriceBreakdownInfo,
+    _spyStateGo;
 
   beforeEach(function() {
     module('underscore');
@@ -55,19 +58,23 @@ describe('RoomProducts', function() {
 
       $provide.value('modalService', {
         openRoomDetailsDialog: function(){},
-        openPoliciesInfo: function(){}
+        openPoliciesInfo: function(){},
+        openPriceBreakdownInfo: function(){}
       });
     });
   });
 
   beforeEach(inject(function($compile, $rootScope, $templateCache,
-      modalService) {
+      $state, modalService) {
     _rootScope = $rootScope.$new();
     _templateCache = $templateCache;
     _templateCache.put(TEMPLATE_URL, TEMPLATE_CONTENT);
     // Spy's
     _spyTemplateCacheGet = sinon.spy(_templateCache, 'get');
     _spyOpenPoliciesInfo = sinon.spy(modalService, 'openPoliciesInfo');
+    _spyOpenRoomDetailsDialog = sinon.spy(modalService, 'openRoomDetailsDialog');
+    _spyOpenPriceBreakdownInfo = sinon.spy(modalService, 'openPriceBreakdownInfo');
+    _spyStateGo = sinon.spy($state, 'go');
 
     // Final component compile
     // Data on parent scope
@@ -91,6 +98,9 @@ describe('RoomProducts', function() {
   afterEach(function() {
     _spyTemplateCacheGet.restore();
     _spyOpenPoliciesInfo.restore();
+    _spyOpenRoomDetailsDialog.restore();
+    _spyOpenPriceBreakdownInfo.restore();
+    _spyStateGo.restore();
   });
 
   describe('when component is initialized', function() {
@@ -102,11 +112,34 @@ describe('RoomProducts', function() {
     it('should compile widget template', function() {
       expect(_elem.html()).equal(TEMPLATE_CONTENT);
     });
+
+    it('should download room products from the server', function(){
+      expect(_scope.products).equal(TEST_ROOM_PRODUCTS.products);
+    });
   });
 
   describe('openRoomDetailsDialog', function(){
     it('should be defined as a function on scope according to settings in config', function(){
       expect(_scope.openRoomDetailsDialog).to.be.a('function');
+    });
+
+    it('should open room details dialog', function(){
+      var testObj = {test: 123};
+
+      _scope.openRoomDetailsDialog(testObj);
+      expect(_spyOpenRoomDetailsDialog.calledOnce).equal(true);
+      expect(_spyOpenRoomDetailsDialog.calledWith(testObj)).equal(true);
+    });
+  });
+
+  describe('openPriceBreakdownInfo', function(){
+    it('should be defined as a function on scope', function(){
+      expect(_scope.openPriceBreakdownInfo).to.be.a('function');
+    });
+
+    it('should open price breakdown dialog', function(){
+      _scope.openPriceBreakdownInfo();
+      expect(_spyOpenPriceBreakdownInfo.calledOnce).equal(true);
     });
   });
 
@@ -120,7 +153,6 @@ describe('RoomProducts', function() {
     });
   });
 
-
   describe('getProductName', function(){
     it('should return first word of a product name', function(){
       expect(_scope.getProductName('testname')).equal('testname');
@@ -132,6 +164,14 @@ describe('RoomProducts', function() {
     it('should return product name without the first word', function(){
       expect(_scope.getProductSuffix('testname')).equal('');
       expect(_scope.getProductSuffix('test name')).equal('name');
+    });
+  });
+
+  describe('selectProduct', function(){
+    it('should redirect to reservation details state', function(){
+      _scope.selectProduct('testRoom', 'testProduct');
+      expect(_spyStateGo.calledOnce).equal(true);
+      expect(_spyStateGo.calledWith('reservation.details')).equal(true);
     });
   });
 });
