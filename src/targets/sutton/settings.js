081926644ecd2ea7e7bc255ceb0183134c827299
Gregory Lebret <greg@lglab.co.uk>
2015-09-21 13:47:20 +0100

    settings.js
    added all sutton defaults back

--- src/targets/sutton/settings.js
+++ src/targets/sutton/settings.js
@@ -96,7 +96,7 @@ angular.module('mobiusApp.config', [])
   'UI': {
     'generics': {
       'singleProperty': false,
-      'loyaltyProgramEnabled': false
+      'loyaltyProgramEnabled': true
     },
     'adverts' : {
       'randomMainPageAdvertSize' : 'homepage-advert',
