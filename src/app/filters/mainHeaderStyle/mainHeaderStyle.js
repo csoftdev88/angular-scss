1b1e45b900ffe0be05404351f439056c0abfc5ba
Andy Pi <lacarrosa@gmail.com>
2015-09-22 20:50:10 -0300

    Added param to tell the filter how many words shold wrap

--- src/app/filters/mainHeaderStyle/mainHeaderStyle.js
+++ src/app/filters/mainHeaderStyle/mainHeaderStyle.js
@@ -7,14 +7,20 @@
 angular.module('mobiusApp.filters.mainHeaderStyle', [])
 
 .filter('mainHeaderStyle' , function() {
-  function filter(input) {
+  function filter(input, wrap) {
     if(!input){
       return '';
     }
+    wrap = (angular.isUndefined(wrap))?1:wrap;
 
     var words = input.split(' ');
-    if(words.length > 1){
-      words[words.length-1] = '<strong>' + words[words.length-1] + '</strong>';
+    if(words.length > wrap){
+      if(wrap === 1){
+        words[words.length-wrap] = '<strong>' + words[words.length-wrap] + '</strong>';
+      }else{
+        words[words.length-wrap] = '<strong>' + words[words.length-wrap];
+        words[words.length-1] = words[words.length-1] + '</strong>';
+      }
     }
     return words.join(' ');
   }
--- src/app/filters/mainHeaderStyle/mainHeaderStyle.spec.js
+++ src/app/filters/mainHeaderStyle/mainHeaderStyle.spec.js
@@ -16,7 +16,12 @@ describe('mainHeaderStyle', function() {
     expect(_mainHeaderStyleFilter()).equal('');
   });
 
-  it('should return properly formatted title and wrap last word into a strong tag', function() {
+  it('should return properly formatted title and wrap words into a strong tag based on the wrap var', function() {
+    expect(_mainHeaderStyleFilter('Title Strong', 3)).equal('Title Strong');
+    expect(_mainHeaderStyleFilter('Title Strong Stronger', 2)).equal('Title <strong>Strong Stronger</strong>');
+  });
+
+  it('should return properly formatted title and wrap last word into a strong tag when wrap is not defined', function() {
     expect(_mainHeaderStyleFilter('Title')).equal('Title');
     expect(_mainHeaderStyleFilter('Title Strong')).equal('Title <strong>Strong</strong>');
     expect(_mainHeaderStyleFilter('Title Strong Stronger')).equal('Title Strong <strong>Stronger</strong>');
