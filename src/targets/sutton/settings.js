1ffbfd0da426db2a3a09d19171951ea17e8a9028
Gregory Lebret <greg@lglab.co.uk>
2016-02-02 12:29:56 +0000

    added missing full stop

--- src/targets/sutton/settings.js
+++ src/targets/sutton/settings.js
@@ -367,7 +367,7 @@ angular.module('mobiusApp.config', [])
           'name': 'Sutton <strong>Prestige</strong>',
           'state': 'aboutUs({code: "prestige"})',
           'pictureUrl': '/static/images/v4/icon-discounts.png',
-          'text': 'Accumulate points for each dollar spent and earn bonus points. Redeem them for unique gifts & rewards with our reward program'
+          'text': 'Accumulate points for each dollar spent and earn bonus points. Redeem them for unique gifts & rewards with our reward program.'
         },
         {
           'name': 'Exciting <strong>News</strong>',
