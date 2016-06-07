f5aadf784d4924deb6032a9f1f8ab4fc8d985255
Gregory Lebret <greg@lglab.co.uk>
2016-06-07 09:59:37 +0100

    added region detail markup

--- src/app/controllers/regions/regions.js
+++ src/app/controllers/regions/regions.js
@@ -4,16 +4,30 @@
  */
 angular.module('mobius.controllers.regions', [])
 
-  .controller('RegionsCtrl', function($scope, locationService, breadcrumbsService, $stateParams) {
+  .controller('RegionsCtrl', function($scope, locationService, breadcrumbsService, $stateParams, scrollService, $timeout) {
 
     breadcrumbsService.addBreadCrumb('Locations');
 
     $scope.showDetail = $stateParams.code ? true : false;
     $scope.allRegions = null;
 
+    $scope.$watch(function(){
+      return $scope.showDetail;
+    }, function(){
+      if($scope.showDetail) {
+        $timeout(function () {
+          scrollService.scrollTo('region-detail', 20);
+        });
+      }
+    });
+
     //Get Regions
     locationService.getRegions().then(function(regions){
       $scope.allRegions = regions;
+
+      if ($stateParams.code) {
+        //selectOffer(bookingService.getCodeFromSlug($stateParams.code));
+      }
     });
 
 
