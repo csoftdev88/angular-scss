56031a0998d01c2ec7849d6aa9134ae51ac8285e
Luciano Fantone <lfantone@users.noreply.github.com>
2016-09-08 08:57:51 -0300

    Convert nights into string

--- src/app/services/mobiusTrackingService/mobiusTrackingService.js
+++ src/app/services/mobiusTrackingService/mobiusTrackingService.js
@@ -151,7 +151,7 @@ angular.module('mobiusApp.services.mobiusTrackingService', []).service('mobiusTr
       _.each(products, function (product) {
         _.each(product.price.breakdowns, function (night) {
           nightObj = {
-            'date': night.date,
+            'date': _.isNumber(night.date) ? new Date(night.date).toISOString() : night.date,
             'rate': {
               'currency': $rootScope.currencyCode,
               'amount': night.totalAfterTax
