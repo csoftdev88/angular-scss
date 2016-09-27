0f03181d8911eff8db0cb68b73f817048189be8f
Christopher Miles <chris@2pventures.com>
2016-09-27 09:44:38 +0100

    Ensure channel service correctly stores ID in cookie

--- src/app/services/channelService/channelService.js
+++ src/app/services/channelService/channelService.js
@@ -6,10 +6,11 @@ angular.module('mobiusApp.services.channelService', [])
   .service('channelService', function(_, Settings, stateService, $location, $window, cookieFactory) {
 
     function getChannel() {
+      var cookieName = Settings.API.sessionData.channelIdCookie;
+
       //If our querystring contains the param ch=me then select the channelId for meta
       if ($location.search().ch && $location.search().ch === 'me') {
         //Store the channel ID in a session cookie and then return the channel
-        var cookieName = Settings.API.sessionData.channelIdCookie;
         var metaChannel = _.find(Settings.API.headers['Mobius-channelId'], function(channel) {
           return channel.name === 'meta';
         });
@@ -26,13 +27,21 @@ angular.module('mobiusApp.services.channelService', [])
       //If not and if we are in mobile view, return the id for mobile
       else if (stateService.isMobileDevice()) {
         return _.find(Settings.API.headers['Mobius-channelId'], function(channel) {
-          return channel.name === 'mobileWeb';
+          if(channel.name === 'mobileWeb')
+          {
+            $window.document.cookie = cookieName + '=' + channel.channelID + '; path=/';
+            return channel.name;
+          }
         });
       }
       //Otherwise return the id for desktop
       else {
         return _.find(Settings.API.headers['Mobius-channelId'], function(channel) {
-          return channel.name === 'web';
+          if(channel.name === 'web')
+          {
+            $window.document.cookie = cookieName + '=' + channel.channelID + '; path=/';
+            return channel.name;
+          }
         });
       }
     }
