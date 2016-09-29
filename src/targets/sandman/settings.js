eef807888118ad662deda1a86abbe06ba4e2641d
Christopher Miles <chris@2pventures.com>
2016-09-29 09:34:12 +0100

    Changing staging and live API end-points to proxy

--- src/targets/sandman/settings.js
+++ src/targets/sandman/settings.js
@@ -65,8 +65,8 @@ angular.module('mobiusApp.config', [])
     'baseURL': {
       'development': ' http://integration-sandman-www.mobiuswebservices.com:3010/api/4.0/',
       'integration': ' http://integration-sandman-www.mobiuswebservices.com:3010/api/4.0/',
-      'staging': ' http://staging-us-node-sandman.mobiuswebservices.com:3010/api/4.0/',
-      'live':  ' https://api.sandmanhotels.com/api/4.0/'
+      'staging': '//staging-us-www-sandman.mobiuswebservices.com/api/4.0/',
+      'live':  'https://www.sandmanhotels.com/api/4.0/'
     },
     'mobiusTracking': {
       'enable': false,
