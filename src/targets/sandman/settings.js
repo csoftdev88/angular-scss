e05e5fa77f467a8c8c395a70289204c023538dfb
Christopher Miles <chris@2pventures.com>
2016-10-19 15:29:51 +0100

    Disable mobius tracking

--- src/targets/sandman/settings.js
+++ src/targets/sandman/settings.js
@@ -69,7 +69,7 @@ angular.module('mobiusApp.config', [])
       'live':  'https://www.sandmanhotels.com/api/4.0/'
     },
     'mobiusTracking': {
-      'enable': true,
+      'enable': false,
       'search': 'properties/track/search',
       'purchase': 'properties/track/purchase'
     },
