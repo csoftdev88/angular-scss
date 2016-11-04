066cab5f2a7fd821036ccb5a628196dfd7618899
Christopher Miles <chris@2pventures.com>
2016-11-04 10:40:38 +0000

    Switching customer ID's

--- src/app/controllers/reservations/reservation.js
+++ src/app/controllers/reservations/reservation.js
@@ -1351,8 +1351,8 @@ angular.module('mobius.controllers.reservation', [])
       'country': getUserCountry().code
     };
 
-    customerObject.id = cookieFactory('CustomerID') ? parseInt(cookieFactory('CustomerID')) : 0;
-    customerObject.infinitiId = user.isLoggedIn() ? user.getCustomerId() : '';
+    customerObject.id = cookieFactory('CustomerID') ? cookieFactory('CustomerID') : 0;
+    customerObject.infinitiId = user.isLoggedIn() ? user.getCustomerId().toString() : '';
 
     var rooms = [];
     _.each($scope.allRooms, function(roomData,index) {
