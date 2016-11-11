01a732bd195d9c83797da773d327b5169f386c20
Christopher Miles <chris@2pventures.com>
2016-11-11 16:03:31 +0000

    Enable for integration

--- src/targets/sandman/settings.js
+++ src/targets/sandman/settings.js
@@ -55,7 +55,7 @@ angular.module('mobiusApp.config', [])
       'password': 'nzLbJUo0h9Gg96NeNcT4Fu2+DyEgX7wk'
     },
     'integration': {
-      'enable':false,
+      'enable':true,
       'endpoint':'https://xozl9li01g.execute-api.us-east-1.amazonaws.com/latest/ecommerce',
       'scriptUrl':'//fcbloyalty.infiniti.io/apeiron/1.01/infiniti-track2.min.js',
       'id':'sandman-integration',
