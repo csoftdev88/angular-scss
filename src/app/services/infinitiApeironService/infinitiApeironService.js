2dd7c35d38dcce9440c104b59be6d169ebe13b6e
Christopher Miles <chris@2pventures.com>
2016-11-15 16:44:32 +0000

    Added nights array to meta

--- src/app/services/infinitiApeironService/infinitiApeironService.js
+++ src/app/services/infinitiApeironService/infinitiApeironService.js
@@ -269,6 +269,16 @@ angular.module('mobiusApp.services.infinitiApeironService', []).service('infinit
           productPolicies.push(policy);
         });
 
+        var nights = [];
+        _.each(product.price.breakdowns, function(breakdown) {
+          var night = {
+            'totalRevenue': breakdown.totalBaseAfterPricingRules,
+            'totalPrice': breakdown.totalAfterTaxAfterPricingRules,
+            'totalTax': breakdown.totalTax + breakdown.totalFees,
+          };
+          nights.push(night);
+        });
+
         var localeData = propertyData.locale.split('-');
 
         var result = {
@@ -310,7 +320,8 @@ angular.module('mobiusApp.services.infinitiApeironService', []).service('infinit
             'property': {
               'code': propertyData.code,
               'name': propertyData.nameShort
-            }
+            },
+            'nights': nights
           },
           'product': {
             'id': product.code,
