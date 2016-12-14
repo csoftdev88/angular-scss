a5a4ece481cf675f4f8c29e30bea96b5ff74e1ae
Luciano Fantone <lucianof@2PVentures.com>
2016-12-14 15:33:18 -0300

    Enable thirdparties

--- src/targets/sandman/settings.js
+++ src/targets/sandman/settings.js
@@ -1089,7 +1089,7 @@ angular.module('mobiusApp.config', [])
       }
     ],
     'thirdparties': {
-      'enable': false,
+      'enable': true,
       'menu': {
         'showHotels': false,
         'showRegionsMegaMenu': true,
