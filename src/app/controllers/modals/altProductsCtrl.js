4f639bb623a8022dbf06398603f995df20197535
Christopher Miles <chris@2pventures.com>
2017-02-28 12:44:12 +0000

    Adding rules for no arrival on and no departure on.
    Either push back the arrival date or the departure date if the day of
    the week matches that in the partialAvailability

--- src/app/controllers/modals/altProductsCtrl.js
+++ src/app/controllers/modals/altProductsCtrl.js
@@ -7,7 +7,7 @@ angular.module('mobius.controllers.modals.altProducts', [
   'mobius.controllers.common.sanitize'
 ])
 
-.controller('AltProductsCtrl', function($scope, $modalInstance, $controller, $state, $stateParams, $window, $filter, bookingService, data) {
+.controller('AltProductsCtrl', function($scope, $modalInstance, $controller, $state, $stateParams, $window, $filter, bookingService, Settings, data) {
   $controller('PriceCtr', {$scope: $scope});
   $controller('SanitizeCtrl', {$scope: $scope});
   $controller('ModalCtrl', {$scope: $scope, $modalInstance: $modalInstance});
@@ -22,7 +22,7 @@ angular.module('mobius.controllers.modals.altProducts', [
           //Check if length of stay is less than min length of stay
           if(lengthOfStay < partialAvailability.variable){
             $scope.stayExtension = partialAvailability.variable - lengthOfStay;     
-            bookingParams.dates = $window.moment(bookingDates.from).format('YYYY-MM-DD') + '_' + $window.moment(bookingDates.to).add($scope.stayExtension, 'days').format('YYYY-MM-DD');
+            bookingParams.dates = $window.moment.tz(bookingDates.from, Settings.UI.bookingWidget.timezone).format('YYYY-MM-DD') + '_' + $window.moment.tz(bookingDates.to, Settings.UI.bookingWidget.timezone).add($scope.stayExtension, 'days').format('YYYY-MM-DD');
           }
         }     
         break;
@@ -32,30 +32,60 @@ angular.module('mobius.controllers.modals.altProducts', [
           //Check if length of stay is more than max length of stay
           if(lengthOfStay > partialAvailability.variable){
             $scope.stayReduction = lengthOfStay - partialAvailability.variable;     
-            bookingParams.dates = $window.moment(bookingDates.from).format('YYYY-MM-DD') + '_' + $window.moment(bookingDates.to).subtract($scope.stayReduction, 'days').format('YYYY-MM-DD');
+            bookingParams.dates = $window.moment.tz(bookingDates.from, Settings.UI.bookingWidget.timezone).format('YYYY-MM-DD') + '_' + $window.moment.tz(bookingDates.to, Settings.UI.bookingWidget.timezone).subtract($scope.stayReduction, 'days').format('YYYY-MM-DD');
           }
         }     
         break;
       //Stay allowed after (date)
       case 'dp':
         if(bookingDates && partialAvailability.date){
-          var allowedFromDate = $window.moment(partialAvailability.date).add(1,'day');
-          $scope.stayReduction = allowedFromDate.diff($window.moment(bookingDates.from), 'days');
+          allowedFromDate = $window.moment.tz(partialAvailability.date, Settings.UI.bookingWidget.timezone).add(1,'day');
+          $scope.stayReduction = allowedFromDate.diff($window.moment.tz(bookingDates.from, Settings.UI.bookingWidget.timezone), 'days');
           //Set the 'from' date to the date passed to us in the partialAvailability 
-          bookingParams.dates = allowedFromDate.format('YYYY-MM-DD') + '_' + $window.moment(bookingDates.to).format('YYYY-MM-DD');
+          bookingParams.dates = allowedFromDate.format('YYYY-MM-DD') + '_' + $window.moment.tz(bookingDates.to, Settings.UI.bookingWidget.timezone).format('YYYY-MM-DD');
           $scope.fromDate = allowedFromDate.format('YYYY-MM-DD');
-          $scope.toDate = $window.moment(bookingDates.to).format('YYYY-MM-DD');
+          $scope.toDate = $window.moment.tz(bookingDates.to, Settings.UI.bookingWidget.timezone).format('YYYY-MM-DD');
         }     
         break;
       //Stay allowed before (date)
       case 'dl':
         if(bookingDates && partialAvailability.date){
-          var allowedToDate = $window.moment(partialAvailability.date).subtract(1,'day');
-          $scope.stayReduction = allowedToDate.diff($window.moment(bookingDates.to), 'days');
+          allowedToDate = $window.moment.tz(partialAvailability.date, Settings.UI.bookingWidget.timezone).subtract(1,'day');
+          $scope.stayReduction = allowedToDate.diff($window.moment.tz(bookingDates.to, Settings.UI.bookingWidget.timezone), 'days');
           //Set the 'to' date to the date passed to us in the partialAvailability 
-          bookingParams.dates = $window.moment(bookingDates.from).format('YYYY-MM-DD') + '_' + allowedToDate.format('YYYY-MM-DD');
-          $scope.fromDate = $window.moment(bookingDates.from).format('YYYY-MM-DD');
+          bookingParams.dates = $window.moment.tz(bookingDates.from, Settings.UI.bookingWidget.timezone).format('YYYY-MM-DD') + '_' + allowedToDate.format('YYYY-MM-DD');
+          $scope.fromDate = $window.moment.tz(bookingDates.from, Settings.UI.bookingWidget.timezone).format('YYYY-MM-DD');
           $scope.toDate = allowedToDate.format('YYYY-MM-DD');
+        }    
+        break;
+      //No Arrival on (day)
+      case 'na':
+        if(bookingDates && partialAvailability.day){
+          var fromDate = $window.moment.tz(bookingDates.from, Settings.UI.bookingWidget.timezone);
+          //mobius partialAvailability classes Sunday as 1, whereas moment.js classes Sunday as 0. So we need to add 1 to the fromDay to bring them inline.
+          var fromDay = fromDate.add(1, 'days').day();
+          if(partialAvailability.day.toString() === fromDay.toString()){
+            //If the booking day of the week matches the no arrival day, bump the fromDate forward 1 day
+            allowedFromDate = fromDate;
+            bookingParams.dates = allowedFromDate.format('YYYY-MM-DD') + '_' + $window.moment.tz(bookingDates.to, Settings.UI.bookingWidget.timezone).format('YYYY-MM-DD');
+            $scope.fromDate = allowedFromDate.format('YYYY-MM-DD');
+            $scope.toDate = $window.moment.tz(bookingDates.to, Settings.UI.bookingWidget.timezone).format('YYYY-MM-DD');
+          }
+        }    
+        break;
+      //No Departure on (day)
+      case 'nl':
+        if(bookingDates && partialAvailability.day){
+          var toDate = $window.moment.tz(bookingDates.to, Settings.UI.bookingWidget.timezone);
+          //mobius partialAvailability classes Sunday as 1, whereas moment.js classes Sunday as 0. So we need to add 1 to the toDay to bring them inline.
+          var toDay = toDate.add(1, 'days').day();
+          if(partialAvailability.day.toString() === toDay.toString()){
+            //If the booking day of the week matches the no arrival day, bump the fromDate forward 1 day
+            allowedToDate = toDate;
+            bookingParams.dates = $window.moment.tz(bookingDates.from, Settings.UI.bookingWidget.timezone).format('YYYY-MM-DD') + '_' + allowedToDate.format('YYYY-MM-DD');
+            $scope.fromDate = $window.moment.tz(bookingDates.from, Settings.UI.bookingWidget.timezone).format('YYYY-MM-DD');
+            $scope.toDate = allowedToDate.format('YYYY-MM-DD');
+          }
         }    
         break;
       default:
@@ -67,10 +97,12 @@ angular.module('mobius.controllers.modals.altProducts', [
   $scope.data = data;
   var bookingParams = angular.copy($stateParams);
   var bookingDates = bookingParams.dates ? bookingService.datesFromString(bookingParams.dates) : null;
-  var lengthOfStay = bookingDates ? $window.moment(bookingDates.to).diff($window.moment(bookingDates.from), 'days') : null;
+  var lengthOfStay = bookingDates ? $window.moment.tz(bookingDates.to, Settings.UI.bookingWidget.timezone).diff($window.moment.tz(bookingDates.from, Settings.UI.bookingWidget.timezone), 'days') : null;
   var orderedProducts = $filter('orderBy')(data.products, 'price.totalAfterTaxAfterPricingRules');
   var lowestProductPrice = orderedProducts[0].price.totalAfterTaxAfterPricingRules;
   $scope.priceDifference = lowestProductPrice - data.product.price.totalAfterTaxAfterPricingRules;
+  var allowedFromDate = null;
+  var allowedToDate = null;
   $scope.stayExtension = null;
   $scope.stayReduction = null;
 
