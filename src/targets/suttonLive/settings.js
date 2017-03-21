66b2611631bdc8143128400198266641edbabc28
Christopher Miles <chris@2pventures.com>
2017-03-21 12:28:37 +0000

    Enabling addon vouchers for sutton and sandman

--- src/targets/suttonLive/settings.js
+++ src/targets/suttonLive/settings.js
@@ -650,7 +650,8 @@ angular.module('mobiusApp.config', [])
         'print': true
       },
       'displayNewsletterCtaOnReservationDetail': false,
-      'reservationDetailPriceBreakdownExtended': false
+      'reservationDetailPriceBreakdownExtended': false,
+      'displayAddonVoucherEntry': true
     },
 
     'aboutHotel': {
