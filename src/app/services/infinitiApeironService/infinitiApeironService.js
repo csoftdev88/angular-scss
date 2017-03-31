348ae67c876a4489db613408ad00fb9a698c84f1
Christopher Miles <chris@2pventures.com>
2017-03-31 11:54:09 +0100

    Add comment

--- src/app/services/infinitiApeironService/infinitiApeironService.js
+++ src/app/services/infinitiApeironService/infinitiApeironService.js
@@ -241,7 +241,7 @@ angular.module('mobiusApp.services.infinitiApeironService', []).service('infinit
         'totalRevenue': totalPrice, // ADD FEES
         'totalPrice': priceData.totalAfterTaxAfterPricingRules,
         'totalTax': priceData.totalTax, //ADD FEES TO THIS
-        'shipping': 'NONE',
+        'shipping': 'NONE', //This must be a string to pass apeiron purachse event validation
         'shippingDuration': null,
         'shippingOption': null,
         'shippingIsSplit': null,
@@ -422,7 +422,7 @@ angular.module('mobiusApp.services.infinitiApeironService', []).service('infinit
         'totalRevenue': null,
         'totalPrice': null,
         'totalTax': null,
-        'shipping': 'NONE',
+        'shipping': 'NONE', //This must be a string to pass apeiron purachse event validation
         'shippingDuration': null,
         'shippingOption': null,
         'shippingIsSplit': null,
