ed51fa594ed95b150d40a229f499c751bc6ae2ca
Christopher Miles <chris@2pventures.com>
2017-04-02 22:04:47 +0100

    German translations for dynamic messages

--- src/targets/meandall/dynamicMessages.js
+++ src/targets/meandall/dynamicMessages.js
@@ -22,19 +22,19 @@ angular.module('mobiusApp.dynamicMessages', [])
     'pet':'Pet'
   },
   'de':{
-    'your_reservation':'<div>Your Reservation <strong>',
-    'was_successfully_cancelled':'</strong> was successfully cancelled.</div>',
-    'you_have_added':'<div>You have added ',
-    'to_your_reservation':' to your reservation</div>',
-    'you_have_added_passbook': '<div>You have successfully added your reservation to passbook.</div>',
-    'sorry_could_not_add_passbook':'<div>Sorry, we could not add reservation to passbook, please try again.</div>',
+    'your_reservation':'<div>Ihre Reservierung <strong>',
+    'was_successfully_cancelled':'</strong> wurde erfolgreich storniert.</div>',
+    'you_have_added':'<div>Sie haben ',
+    'to_your_reservation':' zu Ihrer Reservierung hinzugefügt</div>',
+    'you_have_added_passbook': '<div>Sie haben Ihre Reservierung zum Passbook erfolgreich hinzugefügt.</div>',
+    'sorry_could_not_add_passbook':'<div>Entschuldigung, wir konnten keine Reservierung zum Sparbuch hinzufügen, bitte versuchen Sie es erneut.</div>',
     'exact_dates':'Genaue Daten',
-    'cancellation':'DE Cancellation',
-    'checkInOut':'DE Check-In-Out',
-    'extraGuest':'DE Extra Guest',
-    'family':'DE Family',
-    'guarantee':'DE Guarantee',
-    'noShow':'DE No Show',
-    'pet':'DE Pet'
+    'cancellation':'Stornierung',
+    'checkInOut':'Einchecken / Auschecken',
+    'extraGuest':'Extra Gast',
+    'family':'Familie',
+    'guarantee':'Garantie',
+    'noShow':'Keine Show',
+    'pet':'Haustier'
   }
 });
