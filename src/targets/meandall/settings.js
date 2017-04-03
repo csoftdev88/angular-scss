7ab343ba7291b2ff438f795f50fa4598eab574c0
Christopher Miles <chris@2pventures.com>
2017-04-03 22:32:17 +0100

    Hide send me emails checkbox on steps after details and billing

--- src/targets/meandall/settings.js
+++ src/targets/meandall/settings.js
@@ -987,6 +987,8 @@ angular.module('mobiusApp.config', [])
       'newsLetterOptedIn': true,
       //Display the property name after room details in overview panel
       'propertyNameAfterDetails':true,
+      //Hide newsletter checkbox if not on first step
+      'hideNewsletterCheckboxIfSelected':true,
       //Prompt to ask user to login
       'loginCta':{
         'display': false
