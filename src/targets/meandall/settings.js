b2d31deb4ddd2ae211dcdf5fef2bdd593c489ecb
Christopher Miles <chris@2pventures.com>
2017-04-04 11:38:34 +0100

    Change staging api end-point to meandall

--- src/targets/meandall/settings.js
+++ src/targets/meandall/settings.js
@@ -121,7 +121,7 @@ angular.module('mobiusApp.config', [])
     'baseURL': {
       'development': '//integration-lindner-node.mobiuswebservices.com/api/4.0/',
       'integration': '//integration-lindner-node.mobiuswebservices.com/api/4.0/',
-      'staging': '//staging-api-lindner.mobiusbookingengine.com/api/4.0/',
+      'staging': '//staging-api-meandall.mobiusbookingengine.com/api/4.0/',
       'uat': '//staging-lindner-node.mobiuswebservices.com/api/4.0/',
       'live': '//meandallhotels.com/api/4.0/'
     },
