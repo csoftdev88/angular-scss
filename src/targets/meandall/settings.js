db4df962338b91023aec26ecf9b227eee095aadd
Christopher Miles <chris@2pventures.com>
2017-04-04 01:09:39 +0100

    Changed chainId for me&all

--- src/targets/meandall/settings.js
+++ src/targets/meandall/settings.js
@@ -221,7 +221,7 @@ angular.module('mobiusApp.config', [])
     },
     'headers': {
       // Auth header is set by a static server. See: config/environment/index.js
-      'Mobius-chainId': '2',
+      'Mobius-chainId': '1000',
       'Mobius-channelId': [{
         'name': 'mobileWeb',
         'channelID': 23,
