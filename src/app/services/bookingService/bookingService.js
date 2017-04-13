09cc8fa148d349d444c128220cf8d5e113ecba59
Christopher Miles <chris@2pventures.com>
2017-04-13 09:47:39 +0100

    Re-instate configurable promoCode query param
    Adding ability to override the promoCode param from config with
    something like corpCode.
    
    In Sutton promoCode param is overridden with corpCode when API requests
    are made, this logic has been re-instated.

--- src/app/services/bookingService/bookingService.js
+++ src/app/services/bookingService/bookingService.js
@@ -111,6 +111,8 @@ angular.module('mobiusApp.services.booking', [])
           queryParams[API_PARAM_FROM] = dates.from;
           queryParams[API_PARAM_TO] = dates.to;
         }
+      } else if (key === 'promoCode') {
+        queryParams[getCodeParamName(value)] = value;
       } else if (QUERY_TO_API_PARAMS[key]) {
         queryParams[QUERY_TO_API_PARAMS[key]] = value;
       }
