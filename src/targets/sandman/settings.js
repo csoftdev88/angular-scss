9256e93aa306bc008c45e5ad1793b271a450c502
Christopher Miles <chris@2pventures.com>
2017-04-21 18:01:39 +0100

    Sandman, hiding duplicated product tagline on mobile view

--- src/targets/sandman/settings.js
+++ src/targets/sandman/settings.js
@@ -430,7 +430,7 @@ angular.module('mobiusApp.config', [])
           'ratesPerRoomOnDesktop': 3,
           'ratesPerRoomOnMobile': 2,
           'showTagline': true,
-          'showDescription': true,
+          'showDescription': false,
           'showDescriptionToggle': false,
           'showNoRatesSubDescription': false,
           'showRateInfoIcon': true,
