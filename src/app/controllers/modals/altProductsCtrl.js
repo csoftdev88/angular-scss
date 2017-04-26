c50fd6ce05540eb7e8dc645aae79c57630d2c92f
Christopher Miles <chris@2pventures.com>
2017-04-26 11:00:50 +0100

    Updated checks on dateFroma and dateTo for lengthOfStay

--- src/app/controllers/modals/altProductsCtrl.js
+++ src/app/controllers/modals/altProductsCtrl.js
@@ -99,7 +99,7 @@ angular.module('mobius.controllers.modals.altProducts', [
   var bookingDates = bookingParams.dates ? bookingService.datesFromString(bookingParams.dates) : null;
   var dateFrom = bookingDates && bookingDates.from ? $window.moment.tz(bookingDates.from, Settings.UI.bookingWidget.timezone) : null;
   var dateTo = bookingDates && bookingDates.to ? $window.moment.tz(bookingDates.to, Settings.UI.bookingWidget.timezone) : null;
-  var lengthOfStay = bookingDates ? dateTo.diff(dateFrom, 'days') : null;
+  var lengthOfStay = dateFrom && dateTo ? dateTo.diff(dateFrom, 'days') : null;
   var orderedProducts = $filter('orderBy')(data.products, 'price.totalAfterTaxAfterPricingRules');
   var lowestProductPrice = orderedProducts[0].price.totalAfterTaxAfterPricingRules;
   $scope.priceDifference = lowestProductPrice - data.product.price.totalAfterTaxAfterPricingRules;
