6c231930c51c723e4fe049f53133cd321b755172
Christopher Miles <chris@2pventures.com>
2017-05-09 16:08:24 +0100

    Add catch to previous search modal

--- src/app/controllers/modals/previousSearches.js
+++ src/app/controllers/modals/previousSearches.js
@@ -53,14 +53,18 @@ angular.module('mobius.controllers.modals.previousSearches', [
   $scope.data = data;
 
   $scope.viewSearch = function(search){
-    
-    previousSearchesService.getSearchUrlParams(search).then(function(params){
-      search.params = params;
-      $scope.cancel();
-      //Go to this search URL
-      $state.go(search.s, search.params, {reload: true});
-    });
-    
+
+    previousSearchesService.getSearchUrlParams(search)
+      .then(function(params){
+        search.params = params;
+        $scope.cancel();
+        //Go to this search URL
+        $state.go(search.s, search.params, {reload: true});
+      })
+      .catch(function (err) {
+        console.error(err);
+      });
+
   };
 
   $scope.removeSearch = function(searchToRemove){
