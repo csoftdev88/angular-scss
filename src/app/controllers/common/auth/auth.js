9fd08a4d56e555e03e10eef82324a0070d6c05a0
bkneis <bryan2.kneis@uwe.ac.uk>
2017-05-15 13:57:33 +0100

    Update auth controller to allow for setting of strategy

--- src/app/controllers/common/auth/auth.js
+++ src/app/controllers/common/auth/auth.js
@@ -5,13 +5,54 @@
 angular.module('mobius.controllers.common.auth', [])
 
 .controller( 'AuthCtrl', function($scope, _, user, config) {
+
+  var strategy;
+
+  var isValidStrategy = function (strategy) {
+    return _.isFunction(strategy.login) && _.isFunction(strategy.logout) && _.isFunction(strategy.isLoggedIn);
+  };
+
+  // If the config object contains an auth strategy, and one has not been set yet
+  // then assign the strategy to the controller.
+  if (_.isObject(config.strategy) && !strategy) {
+    if (isValidStrategy(config.strategy)) {
+      strategy = config.strategy;
+    } else {
+      console.warn('WARNING : Potentially unexpected behaviour. The auth strategy does not meet the requirments.');
+    }
+  }
+
+  // Perform authentication callback passed by the config object
   user.authPromise.then(function(isMobiusUser){
     if(_.isFunction(config.onAuthorized)){
       config.onAuthorized(isMobiusUser);
     }
   });
 
+  $scope.login = function() {
+    if (strategy) {
+      return strategy.login();
+    }
+    console.warn('WARNING : Unexpected beahviour, the auth strategy has not been set');
+  };
+
+  $scope.logout = function() {
+    if (strategy) {
+      return strategy.logout();
+    }
+    console.warn('WARNING : Unexpected beahviour, the auth strategy has not been set');
+  };
+
+  $scope.isLoggedIn = function() {
+    if (strategy) {
+      return strategy.isLoggedIn();
+    }
+    console.warn('WARNING : Unexpected beahviour, the auth strategy has not been set');
+  };
+
+  // Invalidate the auth callback when the controller looses scope
   $scope.$on('$destroy', function() {
     config.onAuthorized = _.noop;
   });
+
 });
