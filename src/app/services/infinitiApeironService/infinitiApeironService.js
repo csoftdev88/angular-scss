3f59f81898c02dc1eb4120b524e3b3358799ed3b
bkneis <bryan2.kneis@uwe.ac.uk>
2017-05-19 14:37:44 +0100

    Add condition to check if duplication should happen

--- src/app/services/infinitiApeironService/infinitiApeironService.js
+++ src/app/services/infinitiApeironService/infinitiApeironService.js
@@ -9,6 +9,7 @@ angular.module('mobiusApp.services.infinitiApeironService', []).service('infinit
     var env = document.querySelector('meta[name=environment]').getAttribute('content');
     var endpoint = Settings.infinitiApeironTracking && Settings.infinitiApeironTracking[env] ? Settings.infinitiApeironTracking[env].endpoint : null;
     var duplicationEndpoint = Settings.infinitiApeironTracking && Settings.infinitiApeironTracking[env] ? Settings.infinitiApeironTracking[env].duplicationEndpoint : null;
+    var shouldDuplicate = Settings.infinitiApeironTracking && Settings.infinitiApeironTracking[env] ? Settings.infinitiApeironTracking[env].enableDuplication : false;
     var username = Settings.infinitiApeironTracking && Settings.infinitiApeironTracking[env] ? Settings.infinitiApeironTracking[env].username : null;
     var password = Settings.infinitiApeironTracking && Settings.infinitiApeironTracking[env] ? Settings.infinitiApeironTracking[env].password : null;
     var apeironId = Settings.infinitiApeironTracking && Settings.infinitiApeironTracking[env] ? Settings.infinitiApeironTracking[env].id : null;
@@ -20,10 +21,12 @@ angular.module('mobiusApp.services.infinitiApeironService', []).service('infinit
         apiService.infinitiApeironPost(endpoint, postData, username, password).then(function() {}, function(err) {
           console.log('Infiniti apeiron purchase tracking error: ' + angular.toJson(err));
         });
-        //POST every purchase event to duplication end-point
-        apiService.infinitiApeironPost(duplicationEndpoint, postData, username, password).then(function() {}, function(err) {
-          console.log('Infiniti apeiron duplication endpoint purchase tracking error: ' + angular.toJson(err));
-        });
+        if (shouldDuplicate) {
+          //POST every purchase event to duplication end-point
+          apiService.infinitiApeironPost(duplicationEndpoint, postData, username, password).then(function() {}, function(err) {
+            console.log('Infiniti apeiron duplication endpoint purchase tracking error: ' + angular.toJson(err));
+          });
+        }
       }
     }
 
@@ -35,10 +38,12 @@ angular.module('mobiusApp.services.infinitiApeironService', []).service('infinit
             apiService.infinitiApeironPost(endpoint, postData, username, password).then(function() {}, function(err) {
               console.log('Infiniti apeiron search tracking error: ' + angular.toJson(err));
             });
-            //POST every track search event to duplication end-point
-            apiService.infinitiApeironPost(duplicationEndpoint, postData, username, password).then(function() {}, function(err) {
-              console.log('Infiniti apeiron duplication endpoint search tracking error: ' + angular.toJson(err));
-            });
+            if (shouldDuplicate) {
+              //POST every track search event to duplication end-point
+              apiService.infinitiApeironPost(duplicationEndpoint, postData, username, password).then(function() {}, function(err) {
+                console.log('Infiniti apeiron duplication endpoint search tracking error: ' + angular.toJson(err));
+              });
+            }
           });
         });
       }
