ec436cffdfaba3738f349324e7db5f636bbdbe93
bkneis <bryan2.kneis@uwe.ac.uk>
2017-05-25 16:30:13 +0100

    Create directive to conditionally get keystone script

--- src/app/directives/keystoneScript/keystoneScript.js
+++ src/app/directives/keystoneScript/keystoneScript.js
@@ -2,32 +2,16 @@
 
 angular
   .module('mobiusApp.directives.keystoneScript', [])
-  .directive('infinitiApeironScript', function(Settings) {
+  .directive('keystoneScript', function(Settings) {
     return {
       restrict: 'E',
       scope: {},
-      template: '',
       link: function() {
         if(Settings.authType === 'keystone') {
           var script = document.createElement('script');
-          script.src = Settings.keystone.development;
-          var trackingIdScript = document.createElement('script');
-          var trackingScriptTextString = 'window.infinitiTrack2InstallationId="'+ apeironSettings.id +'";';
-          if(apeironSettings.segmentWriteId){
-            trackingScriptTextString += '\n' + 'window.segmentWriteId="'+ apeironSettings.segmentWriteId +'";';
-          }
-          if(apeironSettings.singlePageApp){
-            trackingScriptTextString += '\n' + 'window.infinitiSinglePageApp=true;';
-          }
-          var trackingScriptText = document.createTextNode(trackingScriptTextString);
-          trackingIdScript.type = 'text/javascript';
-          trackingIdScript.appendChild(trackingScriptText);
-          document.head.appendChild(trackingIdScript);
-
-          var trackingRemoteScript = document.createElement('script');
-          trackingRemoteScript.type = 'text/javascript';
-          trackingRemoteScript.src = Settings.infinitiApeironTracking[env].scriptUrl;
-          document.head.appendChild(trackingRemoteScript);
+          var env = document.querySelector('meta[name=environment]').getAttribute('content');
+          script.src = Settings.keystone[env];
+          document.head.appendChild(script);
         }
       }
     };
