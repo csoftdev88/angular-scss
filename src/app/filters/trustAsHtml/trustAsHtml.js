1bd702170c52cc3e7f75c4e73ca3f778bd9acce6
bkneis <b.kneis2011@gmail.com>
2017-07-07 10:03:09 +0100

    Add check to ensure value is a string before trsusting as html

--- src/app/filters/trustAsHtml/trustAsHtml.js
+++ src/app/filters/trustAsHtml/trustAsHtml.js
@@ -7,6 +7,9 @@ angular.module('mobiusApp.filters.trustAsHtml', [])
 
 .filter('trustAsHtml' , function($sce) {
   return function (text) {
-    return $sce.trustAsHtml(text);
+    if (typeof text === "string") {
+      return $sce.trustAsHtml(text);
+    }
+    return text;
   };
 });
