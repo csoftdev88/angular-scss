982b728a4077d62f7efb47e6d5cb7c41cb735841
bkneis <b.kneis2011@gmail.com>
2017-07-14 12:22:11 +0100

    Remove test task for google fonts

--- Gruntfile.js
+++ Gruntfile.js
@@ -412,10 +412,6 @@ module.exports = function(grunt) {
     'copy:404'
   ]);
 
-  grunt.registerTask('fonts', [
-    'googlefonts'
-  ]);
-
   //production tasks
   grunt.registerTask('release', [
     'production'
