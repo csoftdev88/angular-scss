a6ddbd00729500c677259d419e1511f153bbb3e2
bkneis <b.kneis2011@gmail.com>
2017-07-31 11:27:18 +0100

    remove console logs on sanitize functions

--- src/app/controllers/common/sanitize/sanitize.js
+++ src/app/controllers/common/sanitize/sanitize.js
@@ -7,7 +7,6 @@ angular.module('mobius.controllers.common.sanitize', [])
 .controller( 'SanitizeCtrl', function($scope, $sce, $log) {
   $scope.sanitize = function(content){
     if (typeof content !== "string") {
-      $log.warn('Sanitize didnt get passed a string');
       return '';
     }
     return $sce.trustAsHtml(content);
--- src/app/filters/trustAsHtml/trustAsHtml.js
+++ src/app/filters/trustAsHtml/trustAsHtml.js
@@ -8,7 +8,6 @@ angular.module('mobiusApp.filters.trustAsHtml', [])
 .filter('trustAsHtml' , function($sce, $log) {
   return function (text) {
     if (typeof text !== 'string') {
-      $log.warn('Trust as html didnt get passed a string');
       return '';
     }
     return $sce.trustAsHtml(text);
