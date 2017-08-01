2acf0055e8e7cc8e14f1abb88ff67c6c3b32d7ef
bkneis <b.kneis2011@gmail.com>
2017-08-01 11:31:12 +0100

    Use git and bower instead of local file in vendor directory

--- build.config.js
+++ build.config.js
@@ -97,6 +97,7 @@ module.exports = {
       js: [
         // jquery is a dependency on a lot of other scripts, ensure it is always loaded first
         'bower_components/jquery/dist/jquery.min.js',
+        'bower_components/jquery-ui/dist/jquery-ui.js',
         'vendors/jquery-ui/jquery-ui.js',
         'vendors/chosen/chosen.jquery.min.js',
         'bower_components/raven-js/dist/raven.min.js',
