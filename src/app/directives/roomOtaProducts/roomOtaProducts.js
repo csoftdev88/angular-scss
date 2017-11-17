abe39bbba652c0fdc4d30d25ae61e9987aaefe9d
Julien Bramary <jul.bramary@gmail.com>
2017-11-17 18:34:35 +0000

    Fix OTA products not working as expected

--- src/app/directives/roomOtaProducts/roomOtaProducts.js
+++ src/app/directives/roomOtaProducts/roomOtaProducts.js
@@ -12,24 +12,32 @@ angular.module('mobiusApp.directives.room.otaProducts', [])
     link: function(scope) {
       var otaRatesConfig = Settings.UI.otaRates;
       if (otaRatesConfig) {
-        scope.$watch('vm.otaProducts', function(newValue) {
-          if (newValue !== undefined) {
-            scope.otaLoading = true;
-            scope.otaProductsList = [];
-            //Creating false load effect
-            $timeout(function(){
-              scope.otaLoading = false;
-            }, 2000);
 
-            _.each(otaRatesConfig, function(otaConfig) {
-              var otaProductItem = {};
-              otaProductItem.price = scope.vm.otaProducts[0].price;
-              otaProductItem.logo = otaConfig.logo;
-              otaProductItem.link = otaConfig.link;
-              scope.otaProductsList.push(otaProductItem);
-            });
-          }
-        });
+        // Closure to update otaProducts either coming from scope or scope.room
+        var updateProducts = function (vm) {
+          return function(newValue) {
+            if (newValue !== undefined) {
+              scope.otaLoading = true;
+              scope.otaProductsList = [];
+              //Creating false load effect
+              $timeout(function(){
+                scope.otaLoading = false;
+              }, 2000);
+
+              _.each(otaRatesConfig, function(otaConfig) {
+                var otaProductItem = {};
+                otaProductItem.price = vm.otaProducts[0].price;
+                otaProductItem.logo = otaConfig.logo;
+                otaProductItem.link = otaConfig.link;
+                scope.otaProductsList.push(otaProductItem);
+              });
+            }
+          };
+        };
+
+        // Watch both as we don't have an isolated scope and these can come from either scope or scope.room
+        scope.$watch('otaProducts', updateProducts(scope));
+        scope.$watch('room.otaProducts', updateProducts(scope.room));
       }
     }
   };
--- src/app/directives/roomProducts/roomProducts.js
+++ src/app/directives/roomProducts/roomProducts.js
@@ -107,9 +107,9 @@ angular.module('mobiusApp.directives.room.products', [])
             console.log('products', scope.products);
 
             scope.otaProducts = data.otaProducts;
-            // FIXME: hack for Excelsior that needs OTA products propagated back up, need to refactor to use the dot-notation
-            scope.vm = scope.vm || {};
-            scope.vm.otaProducts = data.otaProducts;
+            // For Excelsior we need OTA products propagated back up into room
+            scope.room = scope.room || {};
+            scope.room.otaProducts = data.otaProducts;
 
             scope.isRequestedRateReturned = data.requestedRateReturned;
 
@@ -119,7 +119,7 @@ angular.module('mobiusApp.directives.room.products', [])
               // Send tracking data to infiniti
               infinitiApeironService.trackRates(
                 scope.products,
-                scope.vm.otaProducts && scope.vm.otaProducts[0] ? scope.vm.otaProducts[0].price : 0,
+                scope.otaProducts && scope.otaProducts[0] ? scope.otaProducts[0].price : 0,
                 scope.room,
                 dataLayerService.getCategoryName(propertyData, scope.room)
               );
